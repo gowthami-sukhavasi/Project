509ed0c1e774f199ae76d66785d6d8a6a5ec4bd9
Gowthami Sukhavasi <gowthami@gowthamis-air.home>
2024-03-20 21:57:02 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const heading = React.createElement("h1", {}, "Hello from React");
+const root = createRoot(document.getElementById("root"));
+
+root.render(heading);
